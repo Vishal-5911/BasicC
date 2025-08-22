e11bb87dee4115566a68af167694b2e42b7185d4
Vishal Bhatia <124044416+Vishal-5911@users.noreply.github.com>
2025-08-22 14:06:19 +0530

    Update EVEN_Number.cpp

--- FINAL CODE/EVEN_Number.cpp
+++ FINAL CODE/EVEN_Number.cpp
@@ -1,23 +1,16 @@
-#include<stdio.h> 
+#include <iostream>
+using namespace std;
 int main(){
-    int i,n;
-    printf("enter the number to check even = ");
-    scanf("%d",&n);     
-   bool iseven= true;
-        for(i=2;i<=n-1;i++){
-        int rem;
-        rem=n%i;
-        if(rem == 0){
-            iseven= false;
-            break;
-        }
-            
-
-    }
-
-    if(iseven == false){
-            printf("number is even");
-    }else
-            printf("number is not even");
-        return 0;
+    int i=2,num=0,n;
+    cout<<"Enter your number to check Even,Odd = ";
+    cin>>n;
+    if (n % i == num)
+     {
+         num++;
+     }
+    if(num == 1)
+            cout<<"Number is Even";
+    else
+            cout<<"Number is Odd";
+return 0;
 }
